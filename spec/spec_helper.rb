57eecc8de77b9c6d0a476e97aa0b1db18958a9ba
edpackard <edpackard81@gmail.com>
2021-08-17 17:17:48 +0100

    setting up testing environment for db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,19 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 require_relative '../app.rb'
+require_relative './setup_test_database'
 
 ENV['RACK_ENV'] = 'test'
+
 Capybara.app = Bookmarks
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console
   ])
 SimpleCov.start
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
